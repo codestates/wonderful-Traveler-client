a02db5aac4e276a3e904100cc58328a74279b8ca
Jaeyoung Paik <jaeyoungpaik@Jaeyoungui-MacBookPro.local>
2020-12-03 10:42:30 +0900

    need to merge with dev

---
+++ client/src/postpage/Postinfo.js
@@ -0,0 +1,18 @@
+import './Postinfo.css';
+import { Component } from 'react';
+
+class Postinfo extends Component {
+    render(){
+        return(
+            <div>
+                <section className="top-section">
+                    <div></div>
+                </section>
+                <section className="info-section">
+                    
+                </section>
+            </div>
+        )
+    }
+}
+export default Postinfo
--- client/src/postpage/Upload.js
+++ client/src/postpage/Upload.js
@@ -1,29 +1,38 @@
 import './Upload.css';
 import { Component } from 'react';
 import SimpleMDE from "react-simplemde-v1";
+import Showdown from 'showdown';
+import ReactHtmlParser from 'react-html-parser';
 
 class Upload extends Component {
     constructor(props){
         super(props);
         this.state = {
             title: null,
-            textValue: ''
+            textValue: null,
+            location: null
         }
     }
    
     handleTitleChange = (e) => {
         this.setState({ title: e.target.value });
-        console.log(document.querySelector('.CodeMirror-code'));
-    };
-    handleValueChange = (event) => {
-        this.setState({ textValue: event.target.value });
     };
 
     handleSubmit = () => {
-
+        let text = document.querySelector('.CodeMirror-code').children;
+        let arr = [];
+        let converter = new Showdown.Converter();
+        for(let i in text){
+            if(text[i].innerText){
+                let t = converter.makeHtml(text[i].innerText);
+                arr.push(t);
+            }
+        }
+        this.setState({ textValue: arr });
     }
 
     render(){
+        console.log(this.state)
         return(
             <div>
                 <section className="st-section">
@@ -36,7 +45,13 @@ class Upload extends Component {
                             onChange={this.handleValueChange}
                             value={this.state.textValue} />
                     </div>
-                    <button value='submit' onClick={()=>this.handleSubmit}></button>
+                    <button className="submit-button" onClick={this.handleSubmit}>버튼</button>
+                    <div>
+                    {this.state.textValue ? <div>{this.state.textValue.map(v => {
+                        return ReactHtmlParser(v)
+                    })}</div>
+                    : null}
+                    </div>
                 </section>
             </div>
         )
