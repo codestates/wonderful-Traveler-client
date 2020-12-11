917ef05059a32aaeb76df8eec339808547df83a7
Jaeyoung Paik <jaeyoungpaik@Jaeyoungui-MacBookPro.local>
2020-12-11 16:36:49 +0900

    prepare to connect with server

--- client/src/mypage/SetPicture.js
+++ client/src/mypage/SetPicture.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { IMGBB_API_KEY } from '../config/config';
+
 class SetAvatar extends Component {
   state = {
     url: "",
@@ -10,8 +11,17 @@ class SetAvatar extends Component {
 
   componentDidMount = () => {
     this.setState({
-    //   view: this.props.info.userinfo.url,
+        view: this.props.info.userinfo.url,
     })
+  }
+
+  handleUpdate = () => {
+    axios.post("http://localhost:8080/user/changeprofile",
+      { picture: this.state.url }, 
+      {withCredentials: true })
+      .then(() => {
+        alert('프로필 이미지가 업데이트 되었습니다.');
+      })
   }
 
   handleInputValue = (event) => {
@@ -32,7 +42,6 @@ class SetAvatar extends Component {
            url: res.data.data.url,
            view: res.data.data.url
         })
-        console.log(this.state)
       })
   }
   
@@ -45,7 +54,7 @@ class SetAvatar extends Component {
         </div>
         <div className="btn">
           <input type="file" name="file" accept="image/*" onChange={this.handleInputValue} className="input-bn"></input>
-          <button className="cbtn" disabled={this.state.url ? false : "disabled"} >변경</button>
+          <button className="cbtn" disabled={this.state.url ? false : "disabled"} onClick={this.handleUpdate}>변경</button>
         </div>
       </div>
     );
